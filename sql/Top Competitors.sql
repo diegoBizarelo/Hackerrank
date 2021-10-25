4730b65684c57facb6a2b98cfd800902925ae8b6
diegoBizarelo <54692838+diegoBizarelo@users.noreply.github.com>
2021-10-25 16:04:30 -0300

    Update Top Competitors.sql
    Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.

--- sql/Top Competitors.sql
+++ sql/Top Competitors.sql
@@ -4,5 +4,5 @@ select h.hacker_id, h.name
     inner join Challenges c on c.challenge_id = sb.challenge_id
     inner join difficulty d on d.difficulty_level = c.difficulty_level and d.score = sb.score
   group by h.name, h.hacker_id
-  having count(h.hacker_id) > 1
+  having count(h.hacker_id) > 1 
   order by count(h.hacker_id) desc, h.hacker_id asc
